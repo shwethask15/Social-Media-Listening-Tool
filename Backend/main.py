7ca506fecab774fa973513df6e61d83d661e4060
shwetha <shwetha.sk@mathco.com>
2024-06-14 11:56:49 +0530

    updated

--- Backend/main.py
+++ Backend/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI # type: ignore
 from Routes.verbatims_list_routes import router as verbatims_list_router
 from Routes.live_verbatims_list_routes import router as live_verbatims_list_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(CORSMiddleware,allow_origins=['*'])
 
 app.include_router(verbatims_list_router,prefix="")
 app.include_router(live_verbatims_list_router,prefix="")
