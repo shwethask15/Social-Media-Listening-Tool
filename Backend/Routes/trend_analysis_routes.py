9317423375bba52171c6b59aa230f39b2618c475
venkatesh <venkatesh.adinani@mathco.com>
2024-06-26 21:45:07 +0530

    trend_analysis service was added and connected it with route

--- Backend/Routes/trend_analysis_routes.py
+++ Backend/Routes/trend_analysis_routes.py
@@ -3,62 +3,12 @@ from database.session import get_db
 from user_auth.auth_bearer import JWTBearer
 from sqlalchemy.orm import Session
 from crud.crud_trend_analysis import verbatim_count,virality_count,sentiment_count,severity_count
+from Services.trend_analysis_services import get_trend_analysis1
 router = APIRouter()
 
 @router.get("/trend_analysis/")
 async def get_trend_analysis(type : str,token : str = Depends(JWTBearer()),db : Session = Depends(get_db)):
-    if type == "verbatim_count":
-        data = verbatim_count.get_all(db=db)
-        temp = {"april":[],"may":[],"june":[]}
-        for i in data:
-            temp1 = i.__dict__
-            # print(temp["april"])
-            if temp1["date"][4] == "4":
-                temp["april"].append(i)
-            elif temp1["date"][4] == "5":
-                temp["may"].append(i)
-            elif temp1["date"][4] == "6":
-                temp["june"].append(i)
-        return temp
-    elif type == "virality_count":
-        data = virality_count.get_all(db=db)
-        temp = {"april":[],"may":[],"june":[]}
-        for i in data:
-            temp1 = i.__dict__
-            # print(temp["april"])
-            if temp1["date"][4] == "4":
-                temp["april"].append(i)
-            elif temp1["date"][4] == "5":
-                temp["may"].append(i)
-            elif temp1["date"][4] == "6":
-                temp["june"].append(i)
-        return temp
-    elif type == "severity_count":
-        data = severity_count.get_all(db=db)
-        temp = {"april":[],"may":[],"june":[]}
-        for i in data:
-            temp1 = i.__dict__
-            # print(temp["april"])
-            if temp1["date"][4] == "4":
-                temp["april"].append(i)
-            elif temp1["date"][4] == "5":
-                temp["may"].append(i)
-            elif temp1["date"][4] == "6":
-                temp["june"].append(i)
-        return temp
-    elif type == "sentiment_count":
-        data = sentiment_count.get_all(db=db)
-        temp = {"april":[],"may":[],"june":[]}
-        for i in data:
-            temp1 = i.__dict__
-            # print(temp["april"])
-            if temp1["date"][4] == "4":
-                temp["april"].append(i)
-            elif temp1["date"][4] == "5":
-                temp["may"].append(i)
-            elif temp1["date"][4] == "6":
-                temp["june"].append(i)
-        return temp
+    return await get_trend_analysis1(type=type,db=db)
         
 
 
