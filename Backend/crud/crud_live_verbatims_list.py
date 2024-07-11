08c1696ca28ceee766f9e2f4779ca791ca91ff8c
venkatesh.adinani <venkatesh.adinani@mathco.com>
2024-07-11 11:08:39 +0530

    Delete Backend/crud directory

--- Backend/crud/crud_live_verbatims_list.py
+++
@@ -1,8 +0,0 @@
-from Crud.base import CRUDBase
-from Models.live_verbatims_list import Live_Verbatims_List
-from Schemas.live_verbatims_list_schema import Live_Verbatims_List_Create
-
-class CRUDLive_Verbatims_list(CRUDBase[Live_Verbatims_List,Live_Verbatims_List_Create]):
-    ...
-
-Live_Verbatims = CRUDLive_Verbatims_list(Live_Verbatims_List)
--- Backend/crud/crud_users_data.py
+++
@@ -1,9 +0,0 @@
-from Crud.base import CRUDBase
-from Models.users_data_model import User_Data
-from Schemas.user_data_schema import User_data_create
-from sqlalchemy.orm import Session
-
-class CRUDUsers(CRUDBase[User_Data,User_data_create]):
-    ...
-
-Users = CRUDUsers(User_Data)
